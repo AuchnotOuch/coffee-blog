2c29e50108d92c17c4829c795c3f37b4e2f100dd
Alex Auch <alexsamuelauch@gmail.com>
2023-03-03 15:33:19 -0700

    added models for coffee and post

---
+++ backend/db/models/coffee.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Coffee extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Coffee.init({
+    name: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    caffeineContent: DataTypes.FLOAT,
+    caffeinePercentage: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'Coffee',
+  });
+  return Coffee;
+};
