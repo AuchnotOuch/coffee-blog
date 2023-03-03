e10cb468c5f1203604d95c2b7bdb9c51dca22e2b
Alex Auch <alexsamuelauch@gmail.com>
2023-03-03 14:13:01 -0700

    bare bones routes for post router completed

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,6 +2,24 @@ import express from "express"
 
 const postRouter = express.Router()
 
+postRouter.post('/', (req, res) => {
+    return res.json('Create post route')
+})
+
+postRouter.delete('/:id', (req, res) => {
+    const id = req.params.id
+    return res.json(`Post with id ${id} successfully deleted`)
+})
+
+postRouter.get("/ping", (req, res) => {
+    return res.json({ "status": "good" })
+})
+
+postRouter.get("/:id", (req, res) => {
+    const id = req.params.id
+
+    return res.json(`Post id: ${id}`)
+})
 
 postRouter.get("/", (req, res) => {
     return res.json("Post Route")
