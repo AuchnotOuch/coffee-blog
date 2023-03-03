22fc1ca01732af899b647888122d4f6fa4aca0d6
Alex Auch <alexsamuelauch@gmail.com>
2023-03-03 13:54:38 -0700

    bare bones coffee router complete with all routes

--- backend/routes/coffee.js
+++ backend/routes/coffee.js
@@ -2,6 +2,26 @@ import express from "express"
 
 const coffeeRouter = express.Router()
 
+coffeeRouter.post('/create', (req, res) => {
+
+    return res.json('Create coffee route')
+})
+
+coffeeRouter.delete('/delete/:id', (req, res) => {
+    const id = req.params.id
+
+    return res.json(`Coffee with id of ${id} successfully deleted`)
+})
+
+coffeeRouter.get('/ping', (req, res) => {
+    return res.json({ "status": "good" })
+})
+
+coffeeRouter.get('/:id', (req, res) => {
+    const id = req.params.id
+    return res.json(`Coffee ID: ${id}`)
+})
+
 coffeeRouter.get('/', (req, res) => {
     return res.json("Coffee Route")
 })
